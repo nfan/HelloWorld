b8812cb7d682ab7f65daefcf160e580d2c1e23ca
Fan, Nan <nfan@adobe.com>
2013-09-21 09:30:05 +0800

    refactor controller and view inheritance refactor list/view style

--- www/js/view/FormView.js
+++ www/js/view/FormView.js
@@ -1,23 +1,27 @@
 define(['jquery', 'backbone', 'underscore',
+        'view/BaseView',
         'library/CRMApp',
         'library/CRMStore',
         'library/CRMUtil',
         'text!view/FormView.html!strip',
         'model/CRMFormData'
         ],
-        function($, Backbone, _, CRMApp, CRMStore, CRMUtil, FormViewTemplate, CRMFormData) {
+        function($, Backbone, _, BaseView, CRMApp, CRMStore, CRMUtil, FormViewTemplate, CRMFormData) {
 
-    var FormView = Backbone.View.extend({
-        el:'body',
+    var FormView = BaseView.extend({
+
         cur_template_id : 0,
         cur_formdata_id : 0,
         state: 'view',
         
-        events: {
-            "click a.linkToList": 'linkToList',
-            "click a.linkToEdit": 'linkToEdit',
-            "click button.submit": 'submit'
-        },
+        events: function(){
+              return _.extend({}, BaseView.prototype.events,{
+                    "click a.linkToList": 'linkToList',
+                    "click a.linkToEdit": 'linkToEdit',
+                    "click button.submit": 'submit'
+              });
+           },
+
         initialize: function() {
             _.bindAll(this, 'render', 'resetFormData', 'fetchAndRender');
         },
@@ -35,7 +39,7 @@ define(['jquery', 'backbone', 'underscore',
             }
             
             if (!CRMUtil.isEmpty(compiled_template) && !CRMUtil.isEmpty(template)) {
-                if (that.state == 'edit') {
+                if (that.state == 'edit' || that.state == 'add') {
                     html = compiled_template.form({template: template, formdata:that.formdata});
                 } else {
                     html = compiled_template.view({template: template, formdata:that.formdata});
@@ -64,25 +68,41 @@ define(['jquery', 'backbone', 'underscore',
                     var that = this;
 
                     var formDataCollection = CRMStore.getFormDataCollection(that.cur_template_id);
-                    that.formdata = formDataCollection.get(that.cur_formdata_id);                                                                            
+                    
+                    if (that.cur_formdata_id == 0) {
+                        that.formdata = new CRMFormData({collection: formDataCollection});
+                        
+                    } else {
+                    
+                        that.formdata = formDataCollection.get(that.cur_formdata_id);
+                    }
                     if (typeof callback == 'function') {
                       callback();
                     }
         },
         
         linkToList: function(evt) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    
                     var that = this;
 
                     CRMApp.getRouter().navigateTo("list/"+that.cur_template_id);
         },
         
         linkToEdit: function(evt) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    
                     var that = this;
 
                     CRMApp.getRouter().navigateTo("form/edit/"+that.cur_template_id+"/"+that.cur_formdata_id);
         },
         
         submit: function(evt) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    
                     var that = this;
                     
                     var formDataCollection = CRMStore.getFormDataCollection(that.cur_template_id);
